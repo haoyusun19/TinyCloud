31a18a29ae3674bfa73a53066b16cf732eaa6429
Haoyu Sun <sunhaoyu2021@gmail.com>
2022-11-24 14:15:07 +0100

    Initial commit

---
+++ src/main/java/com/haoyu/tinycloud/TinyCloudApplication.java
@@ -0,0 +1,13 @@
+package com.haoyu.tinycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haoyu/tinycloud/TinyCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haoyu.tinycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
